0ef46e3ec3cef07b2b433abf3bcf325e1bd322e2
abbasrahmanii <absrn26@gmail.com>
2022-04-25 16:00:36 +0430

    change sass package

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs, setCurrentSong, audioRef, isPlaying, setSongs, libraryStatus }) => {
-    return (
-        <div className={`library ${libraryStatus ? "active-library" : ""}`}>
-            <h2>Library</h2>
-            <div className="library-songs">
-                {songs.map( song => <LibrarySong isPlaying={isPlaying} audioRef={audioRef} setCurrentSong={setCurrentSong} songs={songs} song={song} id={song.id} key={song.id} setSongs={setSongs} /> )}
-            </div>
-        </div>
-    );
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map((song, index) => (
+          <LibrarySong
+            key={index}
+            isPlaying={isPlaying}
+            audioRef={audioRef}
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+            song={song}
+            id={song.id}
+            setSongs={setSongs}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Library;
